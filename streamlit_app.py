0898963221b61c53b342d400045896e16122c519
jannahsy2709-ui <jannahsy2709@gmail.com>
2026-02-01 20:27:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('Structural Health Monitoring')
 
-st.write('Hello world!')
+st.write('This app will compute the deflection of the bridge acquired from moving loads on bridge')
